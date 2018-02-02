6b1638cde2ee75ecc874bcec6f5ebbd4b628802f
Alycen <alsunnid@gmail.com>
2018-02-02 19:19:30 +0000

    simple text to binary converter, although I think it deletes the first character if it == 0... oh well

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import time
-import sys
+print( "TEXT TO BINARY CONVERTER\n" )
 
-print ("Hello World")
+text = raw_input("Please enter text to convert to binary.\n")
+print(' '.join(format(ord(x), 'b') for x in text))
